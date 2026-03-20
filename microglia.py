91cab4344e885f245e9f0e5deb0fd77ad3e1c76d
Edoardo Marcora <edoardo.marcora@gmail.com>
2026-03-20 00:10:19 -0400

    Add microglia prop estimation with multiple mice

--- microglia.py
+++ microglia.py
@@ -4,11 +4,13 @@ from pangolin import interface as pi
 alpha = 2
 beta = 20
 n = 1000
-y_obs = 80
+y_obs = [80, 60, 90, 70, 50]
 
-π = pi.beta(alpha, beta)
-y = pi.binomial(n, π)
+fn = 0.10
+fp = 0.05
+
+π = [pi.beta(alpha, beta) for i in range(len(y_obs))]
+π̃ = [fp + π[i] * (1 - fn - fp) for i in range(len(y_obs))]
+y = [pi.binomial(n, π̃[i]) for i in range(len(y_obs))]
 
 samples = pg.blackjax.sample(π, y, y_obs, niter=10000)
-
-print(samples.mean())
