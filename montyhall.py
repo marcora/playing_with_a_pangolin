5ab27fb99d1442f92889460be475a3c3eea5660f
Edoardo Marcora <edoardo.marcora@gmail.com>
2026-05-09 16:10:05 -0400

    Add Monty Hall problem example

---
+++ montyhall.py
@@ -0,0 +1,19 @@
+import random
+
+doors = {1, 2, 3}
+
+win_keep = 0
+win_switch = 0
+
+M = 1000
+
+for _ in range(M):
+    car = random.choice(list(doors))
+    pick = random.choice(list(doors))
+    monty = random.choice(list(doors - {car, pick}))
+    if car == pick:
+        win_keep += 1
+    else:
+        win_switch += 1
+
+print(f"Keep: {win_keep}, Switch: {win_switch}")
